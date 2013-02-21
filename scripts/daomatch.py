76f80d50e4300a15d4527e1dfa98bddae55bfdd2
Adam Ginsburg <keflavich@gmail.com>
2013-02-20 20:59:27 -0700

    maybe this right!

--- scripts/daomatch.py
+++ scripts/daomatch.py
@@ -7,10 +7,16 @@ import math
 from pyraf import iraf 
 import ccxymatch_ref
 
-def daomatch(image, outprefix, wcscatalog, **kwargs):
-    iraf.noao.digiphot.apphot.findpars(threshold=10,)
-    iraf.noao.digiphot.apphot.datapars(scale=1, fwhmpsf=2.5, sigma=50,
-            datamin=300, datamax=30000)
+def daomatch(image, outprefix, wcscatalog, noiselevel=50, datamin=300,
+        threshold=10, datamax=30000, **kwargs):
+    # these unfortunately bring up menus...
+    # iraf.noao.digiphot.apphot.findpars(threshold=10,)
+    # iraf.noao.digiphot.apphot.datapars(scale=1, fwhmpsf=2.5, sigma=50,
+    #         datamin=300, datamax=30000)
+    iraf.noao.digiphot.apphot.datapars.sigma=noiselevel
+    iraf.noao.digiphot.apphot.datapars.datamin=datamin
+    iraf.noao.digiphot.apphot.datapars.datamax=datamax
+    iraf.noao.digiphot.apphot.findpars.threshold = threshold
     iraf.noao.digiphot.apphot.daofind(image, output=outprefix+".coo",
             starmap=outprefix+".star", skymap=outprefix+".sky",
             verify=False, update=False, verbose=True, interactive=False)
