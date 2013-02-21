c8f133a6a81275f1a549982765b514032c387f85
Adam Ginsburg <keflavich@gmail.com>
2013-02-20 20:44:12 -0700

    fixed some daopars

--- scripts/daomatch.py
+++ scripts/daomatch.py
@@ -8,7 +8,10 @@ from pyraf import iraf
 import ccxymatch_ref
 
 def daomatch(image, outprefix, wcscatalog, **kwargs):
-    iraf.noao.digiphot.apphot.datapars(scale=1,fwhmpsf=2.5,sigma=50,datamin=300,datamax=30000)
+    iraf.noao.digiphot.apphot.findpars(threshold=10,)
+    iraf.noao.digiphot.apphot.datapars(scale=1, fwhmpsf=2.5, sigma=50,
+            datamin=300, datamax=30000)
     iraf.noao.digiphot.apphot.daofind(image, output=outprefix+".coo",
-            starmap=outprefix+".star", skymap=outprefix+".sky")
+            starmap=outprefix+".star", skymap=outprefix+".sky",
+            verify=False, update=False, verbose=True)
     ccxymatch_ref.ccxymatch_ref(image, outprefix+".coo", wcscatalog, **kwargs)
