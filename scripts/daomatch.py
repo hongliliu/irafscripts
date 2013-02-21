1c3ca5e43bc9d2210332f0cd7cb2d5443aacd5f4
Adam Ginsburg <keflavich@gmail.com>
2013-02-20 21:47:58 -0700

    daomatch has ccmap now

--- scripts/daomatch.py
+++ scripts/daomatch.py
@@ -9,7 +9,8 @@ import ccxymatch_ref
 
 def daomatch(image, outprefix, wcscatalog, noiselevel=10, datamin=300,
         threshold=15, datamax=30000, fwhm=4, roundlo=-0.8, roundhi=0.8,
-        sharphi=0.5, **kwargs):
+        sharphi=0.5, interactive=True, lngunits='hours', latunits='degrees',
+        **kwargs):
     # these unfortunately bring up menus...
     # iraf.noao.digiphot.apphot.findpars(threshold=10,)
     # iraf.noao.digiphot.apphot.datapars(scale=1, fwhmpsf=2.5, sigma=50,
@@ -25,4 +26,10 @@ def daomatch(image, outprefix, wcscatalog, noiselevel=10, datamin=300,
     iraf.noao.digiphot.apphot.daofind(image, output=outprefix+".coo",
             starmap=outprefix+".star", skymap=outprefix+".sky",
             verify=False, update=False, verbose=True, interactive=False)
-    ccxymatch_ref.ccxymatch_ref(image, outprefix+".coo", wcscatalog, **kwargs)
+    ccxymatch_ref.ccxymatch_ref(image, outprefix+".coo", wcscatalog,
+            prefix=outprefix+"_", **kwargs)
+
+    iraf.images.imcoords.ccmap(outprefix+"_match.txt", database=outprefix+"_match.db",
+            images=image, results=outprefix+"_ccmap.db", xcolumn=3, ycolumn=4,
+            lngcolumn=1, latcolumn=2, update=update, interactive=interactive,
+            lngrefunits=lngunits, latrefunits=latunits)
